d4ff65f28944afa0cce19bb1e0ae10a37353454f
Matt <63558223+mcline1980@users.noreply.github.com>
2020-04-20 13:00:59 -0400

    Updated player class object. Implemented a function to create the user. Started a function to create a dictonary object for created users. Started a function to save the created user to a text file.

--- Function_UserCreator.py
+++ Function_UserCreator.py
@@ -2,21 +2,24 @@ import random
 import inspect
 
 
+# Player class. Currently contains default values.
 class Player(object):
-    def __init__(self):
-        self.char_id = 0
-        self.char_sn = 0
-        self.char_max_health = 20
-        self.char_life = 20
-        self.char_try = 20
-        self.name = ""
+    def __init__(self, char_id, char_sn, char_max_health, char_life, char_try, name):
+        self.name = name
+        self.char_id = int(char_id)
+        self.char_sn = int(char_sn)
+        self.char_max_health = int(char_max_health)
+        self.char_life = int(char_life)
+        self.char_try = int(char_try)
 
 
-def user_create():
+# The primary user creation function. Currently set to arbitrary default values to feed into the game.
+# Once game logic is worked out, we can use this to change defaults.
+def user_create(char_sn, char_max_health, char_life):
     # User Defaults
-    Player.char_sn = 19
-    Player.char_max_health = 20
-    Player.char_life = 20
+    Player.char_sn = char_sn
+    Player.char_max_health = char_max_health
+    Player.char_life = char_life
     Player.char_try = 0
     Player.name = input("Please enter your character name: ")
     print(Player.name)
@@ -27,8 +30,31 @@ def user_create():
 
 
 # Test user creation function
+# Debugging use only
 def user_display():
     for i in inspect.getmembers(Player):
         if not i[0].startswith('_'):
             if not inspect.ismethod(i[1]):
                 print(i)
+
+
+# Create a string to feed into a dictionary indexing the player value.
+# To be used for follow on processing/features.
+def user_create_string():
+    string = [Player.name, Player.char_life, Player.char_try, Player.char_max_health, Player.char_sn]
+    user_dict = {Player.char_id: [string]}
+    return user_dict
+
+
+# Write the newly created user to a local text file.
+# To be used for creating new features in the future.
+def user_write_file(user_dict):
+    with open("users.txt", "a") as file:
+        file_content = file
+        file_content.writelines("\n" + user_dict)
+
+
+# Testing the functions
+#user_create()
+#user_create_string()
+#user_write_file(str(user_create_string()))
--- Game.py
+++ Game.py
@@ -1,8 +1,13 @@
-from Function_UserCreator import Player
+from Function_UserCreator import Player  # Import player class object from file
 from Function_UserCreator import user_create
+import random
 
-secret_number = 19  # Temporary placeholder before random number generator is created
-user_create()
+# 1) Pass char_sn -- the secret number
+# 2) Pass char_max_health -- the maximum number of chances the character can earn
+# 3) Pass char_life -- the starting health of the character
+# Define the character for the game
+sn = 19
+user_create(sn, 10, 5)
 
 
 # Define Player Input Function with error handling
@@ -25,23 +30,27 @@ def number_input():
 
 
 # Start Game
+# While the players health is positive
+# If the player guesses above the range, they take a 'hit' and do not gain an attempt.
+# If the player guesses below the range, they earn an attempt.
+# The game runs until the attempts (Player.char_life) run out or the player guesses the number.
+
+secret_number = Player.char_sn
+win = False
 
 while Player.char_life > 0:
-
     # INPUT
     number = number_input()
     # INPUT
-
     Player.char_try += 1
     Player.char_life -= 1
-
     if number < secret_number:
         Player.char_life += 2
         print(
             f"You have guessed below the range, you gain an attempt. You now have {Player.char_life} attempts remaining.")
     if Player.char_life > Player.char_max_health:
         print(f"You have reached the maximum number of attempts. Please keep guessing.")
-        Player.char_life = 10
+        Player.char_life = Player.char_max_health
     if number == secret_number:
         print("You win!")
         break
